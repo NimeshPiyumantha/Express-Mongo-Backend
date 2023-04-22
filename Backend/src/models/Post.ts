8e0f3b7da65ee9b448fdffab8cc13d64c380cbb1
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-04-22 20:40:49 +0530

    update content

---
+++ Backend/src/models/Post.ts
@@ -0,0 +1,38 @@
+import { Document, Schema, model } from "mongoose";
+
+export interface IPost extends Document {
+  title: string;
+  description: string;
+  hoursCount: number;
+  lecturerName: string;
+  tags: string[];
+}
+
+// creating schema
+const PostSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    hoursCount: {
+      type: Number,
+      required: true,
+    },
+    lecturerName: {
+      type: String,
+      required: true,
+    },
+    tags: {
+      type: Array,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Post = model<IPost>("Post", PostSchema);
