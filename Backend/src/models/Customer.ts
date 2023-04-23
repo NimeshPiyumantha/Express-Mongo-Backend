c87188cea4b21576a477d5adc8c4d510f1266306
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-04-23 13:57:08 +0530

    update content

--- Backend/src/models/Customer.ts
+++ Backend/src/models/Customer.ts
@@ -1,16 +1,33 @@
 import { Document, Schema, model } from "mongoose";
 
-export interface IUser extends Document {
+export interface ICustomer extends Document {
   id: string;
   name: string;
   address: string;
   salary: number;
 }
 
-//creating schema
-const UserSchema = new Schema({
-  id: {
-    type: String,
-    required: true,
+// creating schema
+const CustomerSchema = new Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    salary: {
+      type: Number,
+      required: true,
+    },
   },
-});
+  { timestamps: true }
+);
+
+export const Customer = model<ICustomer>("Customer", CustomerSchema);
