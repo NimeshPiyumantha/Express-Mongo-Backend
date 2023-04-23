b4d0f57dcf642fcc2885502955478c5e99877685
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-04-23 10:35:42 +0530

    Add PostRoutes.ts

---
+++ Backend/src/routes/PostRoutes.ts
@@ -0,0 +1,29 @@
+import express, { Router } from "express";
+import PostController from "../controllers/PostController";
+
+export default class PostRoutes {
+  private router: Router = express.Router();
+  private postController: PostController = new PostController();
+
+  constructor() {
+    this.configRoutes();
+  }
+
+  private configRoutes = (): void => {
+    // POST /api/v1/post
+    this.router.post("/", this.postController.createPost);
+
+    // GET /api/v1/post
+    this.router.get("/", this.postController.retrieveAllPosts);
+
+    // PUT /api/v1/post/:id
+    this.router.put("/:id", this.postController.updatePost);
+
+    // DELETE /api/v1/post/:id
+    this.router.delete("/:id", this.postController.deletePost);
+  };
+
+  public getRouter = (): Router => {
+    return this.router;
+  };
+}
