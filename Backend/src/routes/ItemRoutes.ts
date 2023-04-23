a5d1293a9e7cbfcf18ba9f88725b38f478f1e414
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-04-23 16:52:10 +0530

    update content

---
+++ Backend/src/routes/ItemRoutes.ts
@@ -0,0 +1,29 @@
+import express, { Router } from "express";
+import ItemController from "../controllers/ItemController";
+
+export default class ItemRoutes {
+  private router: Router = express.Router();
+  private itemController: ItemController = new ItemController();
+
+  constructor() {
+    this.configRoutes();
+  }
+
+  private configRoutes = (): void => {
+    // POST /api/v1/post
+    this.router.post("/", this.itemController.addNewItem);
+
+    // GET /api/v1/post
+    this.router.get("/", this.itemController.retrieveAllItems);
+
+    // PUT /api/v1/post/:id
+    this.router.put("/:code", this.itemController.updateItem);
+
+    // DELETE /api/v1/post/:id
+    this.router.delete("/:code", this.itemController.deleteItem);
+  };
+
+  public getRouter = (): Router => {
+    return this.router;
+  };
+}
