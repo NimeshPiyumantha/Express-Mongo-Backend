608a30427f1b9f710d0961c9368c100002721ccf
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-04-23 13:50:34 +0530

    update content

--- Backend/src/models/Customer.ts
+++ Backend/src/models/Customer.ts
@@ -1,8 +1,16 @@
-import {Document,Schema,model} from "mongoose";
+import { Document, Schema, model } from "mongoose";
 
-export interface IUser extends Document{
-    id:string;
-    name:string;
-    address:string;
-    salary:number;
+export interface IUser extends Document {
+  id: string;
+  name: string;
+  address: string;
+  salary: number;
 }
+
+//creating schema
+const UserSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+});
