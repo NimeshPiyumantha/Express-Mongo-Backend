3ccac4442a95ccbcaec9ce2f737f3b5554e0b58c
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-04-23 13:32:00 +0530

    update content

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -2,18 +2,37 @@ import { config } from "dotenv";
 // dotenv configuration
 config();
 
-import express from "express";
+import express, { Request, Response } from "express";
 import db from "mongoose";
+import routes from "./routes";
+import { json, urlencoded } from "body-parser";
 
+// Create the express app
 const app = express();
 
+// if you are receiving JSON data in request-body
+app.use(json());
+
+// if you are receiving url-encoded data in request-body
+app.use(urlencoded({ extended: true }));
+
+// Mount the routes at /resourse URL path
+app.use("/", routes);
+
+// error handling middleware
+app.use((error: Error, req: Request, res: Response) => {
+  res.status(500).json({ message: error.message });
+});
+
+// connect to the database
 db.connect(process.env.MONGO_DB_URL!)
   .then(() => {
-    console.log("Database Connected..!");
+    console.log("Database connected!");
+
     app.listen(process.env.PORT, () => {
-      console.log("Server Liestening on Port 5000");
+      console.log("Server is listening on Port 4000");
     });
   })
   .catch((error) => {
-    console.log("Failed to Connect With MongoDb : " + error);
+    console.log("Failed to connect to MongoDB : ", error.message);
   });
