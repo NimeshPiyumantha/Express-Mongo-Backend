a2440f59f68479e918d2bebe5ff8dc639dd49ea6
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-04-23 16:55:21 +0530

    update content

---
+++ Backend/src/routes/OrderRoutes.ts
@@ -0,0 +1,29 @@
+import express, { Router } from "express";
+import OrderController from "../controllers/OrderController";
+
+export default class OrderRoutes {
+  private router: Router = express.Router();
+  private orderController: OrderController = new OrderController();
+
+  constructor() {
+    this.configRoutes();
+  }
+
+  private configRoutes = (): void => {
+    // POST /api/v1/post
+    this.router.post("/", this.orderController.addNewOrder);
+
+    // GET /api/v1/post
+    this.router.get("/", this.orderController.retrieveAllOrders);
+
+    // PUT /api/v1/post/:id
+    this.router.put("/:id", this.orderController.updateOrder);
+
+    // DELETE /api/v1/post/:id
+    this.router.delete("/:id", this.orderController.deleteOrder);
+  };
+
+  public getRouter = (): Router => {
+    return this.router;
+  };
+}
